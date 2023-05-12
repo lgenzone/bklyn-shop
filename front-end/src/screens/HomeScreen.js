03e1c8547ccc6336c96e7a45a5dea97114b37e7e
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-05-11 17:47:14 -0700

    installed axios, added proxy, added fetchData function to front-end home

--- front-end/src/screens/HomeScreen.js
+++ front-end/src/screens/HomeScreen.js
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import data from "../data";
+import axios from "axios";
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get("/api/products");
+        setProducts(result.data);
+      } catch (error) {
+        // Handle the error
+        console.error(error);
+        // You can show an error message or perform any other actions here
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
