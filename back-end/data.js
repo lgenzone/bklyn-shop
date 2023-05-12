df37983451944a303b17596d7dd7b03e8a722f21
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-05-11 17:10:36 -0700

    setup back-end, server.js, i express, nodemon

---
+++ back-end/data.js
@@ -0,0 +1,50 @@
+const data = {
+    products: [
+        {
+            name: 'Beard Oil + Hyaluronic Serum',
+            slug: 'beard-oil',
+            category: 'Serums',
+            image: '/images/p2.png',
+            price: 80,
+            countInStock: 10,
+            rating: '5',
+            numReviews: '10',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        },
+        {
+            name: 'Cherry Enzyme Wash',
+            slug: 'cherry-enzyme-wash',
+            category: 'Facewash',
+            image: '/images/p1.png',
+            price: 75,
+            countInStock: 10,
+            rating: '5',
+            numReviews: '10',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        },
+        {
+            name: 'Acne Wash',
+            slug: 'acne-wash',
+            category: 'Facewash',
+            image: '/images/p1.png',
+            price: 75,
+            countInStock: 20,
+            rating: '5',
+            numReviews: '10',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        },
+        {
+            name: 'Ultra Firm Peptide',
+            slug: 'ultra-firm-peptide',
+            category: 'Cream',
+            image: '/images/p1.png',
+            price: 100,
+            countInStock: 30,
+            rating: '5',
+            numReviews: '10',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        },
+    ],
+}
+
+export default data;
