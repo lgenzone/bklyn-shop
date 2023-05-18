df7c56c6b841315753b1bea2b54bcd7fc4c88fe5
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-05-17 20:41:59 -0700

    import font awesome, added rating and product components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
--- front-end/src/screens/HomeScreen.js
+++ front-end/src/screens/HomeScreen.js
@@ -1,17 +1,20 @@
 import { useEffect, useReducer } from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import axios from "axios";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from "../components/Product";
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'FETCH_REQUEST':
-      console.log('FETCH_REQUEST');
+    case "FETCH_REQUEST":
+      console.log("FETCH_REQUEST");
       return { ...state, loading: true };
-    case 'FETCH_SUCCESS':
-      console.log('FETCH_SUCCESS');
+    case "FETCH_SUCCESS":
+      console.log("FETCH_SUCCESS");
       return { ...state, products: action.payload, loading: false };
-    case 'FETCH_FAIL':
-      console.log('FETCH_FAIL');
+    case "FETCH_FAIL":
+      console.log("FETCH_FAIL");
       return { ...state, loading: false, error: action.payload };
     default:
       return state;
@@ -22,17 +25,17 @@ function HomeScreen() {
   const [{ loading, error, products }, dispatch] = useReducer(reducer, {
     products: [],
     loading: true,
-    error: '',
+    error: "",
   });
 
   useEffect(() => {
     const fetchData = async () => {
-      dispatch({ type: 'FETCH_REQUEST' });
+      dispatch({ type: "FETCH_REQUEST" });
       try {
         const result = await axios.get("/api/products");
-        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (error) {
-        dispatch({ type: 'FETCH_FAIL', payload: error.message });
+        dispatch({ type: "FETCH_FAIL", payload: error.message });
       }
     };
     fetchData();
@@ -47,22 +50,14 @@ function HomeScreen() {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => (
-            <div className="product" key={product.slug}>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+              <Product product={product}>
+              </Product>
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
