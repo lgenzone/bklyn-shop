3a0de8c9e919b47300c90c70e2081bbedff20ef9
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-05-25 17:18:37 -0700

    created User model

--- back-end/data.js
+++ back-end/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: 'Lauren',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: true,
+        },
+        {
+            name: 'Becca',
+            email: 'becca@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: false,
+        },
+
+    ],
     products: [
         {
             //_id: '1',
