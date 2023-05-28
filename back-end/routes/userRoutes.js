c7fe299f319a7f17dab5e285321e2958c6a3a487
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-05-27 19:45:48 -0700

    created sign up screen, backend api, routes

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -13,18 +13,37 @@ userRouter.post(
     if (user) {
       if (user) {
         if (bcrypt.compareSync(req.body.password, user.password)) {
-            res.send({
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                isAdmin: user.isAdmin,
-                token: generateToken(user)
-            });
-            return;
+          res.send({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user),
+          });
+          return;
         }
       }
       res.status(401).send({ message: 'Invalid credentials' });
     }
+  })
+);
+
+userRouter.post(
+  '/signup',
+  expressAsyncHandler(async (req, res) => {
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password),
+    });
+    const user = await newUser.save();
+    res.send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user),
+    });
   })
 );
 
